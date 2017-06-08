82c37f7620cf7cad612424c0398b06bb77e08e88
tol <tol@Mac-mini-tol.local>
2017-06-08 17:54:24 +0300

    added antispam field for contact form

--- contact.php
+++ contact.php
@@ -7,16 +7,22 @@ $to='mel198629@gmail.com';
 $subject='Заказы с сайта ArtOko';
 
 $message ="<html><head><title>Сообщение с сайта sketchbooky.info</title></head><body> 
-       		<table cellpadding='0' cellspacing='0'>
-       			<tr><td style='font-size:16px;>Имя</td><td style='font-size:14px;'>". $name ."</td></tr>
-       			<tr><td style='font-size:16px;>E-mail</td><td style='font-size:14px;'>". $email . "</td> </tr>
-       			<tr><td style='font-size:16px;>Сообщение: </td><td style='font-size:14px;'>". $messageText . "</td> 
-            </tr></table></body></html>";
+    <table cellpadding='0' cellspacing='0'>
+      <tr><td style='font-size:16px;>Имя</td><td style='font-size:14px;'>". $name ."</td>
+      </tr>
+      <tr><td style='font-size:16px;>E-mail</td><td style='font-size:14px;'>". $email . "</td>
+      </tr>
+      <tr><td style='font-size:16px;>Сообщение: </td><td style='font-size:14px;'>". $messageText . "</td> 
+      </tr>
+    </table>
+  </body>
+	</html>";
 
 $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";           
-            
-mail($to,$subject,$message,$headers);
 
+if(isset($_POST['url']) && $_POST['url'] == ''){      
+	mail($to,$subject,$message,$headers);
+}
 header('Location: index.html');
 ?>
