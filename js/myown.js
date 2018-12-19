b6b1f19d3a4f7824b30f77bd565db7345273df67
menim <mel198629@gmail.com>
2018-12-19 16:11:33 +0200

    add smooth reveal of nav on mobile device when navbar-toggler was clicked

--- js/myown.js
+++ js/myown.js
@@ -1,6 +1,14 @@
+//precommit eslint settings for package json
+      // "*.js": [
+      //   "npm run prettier",
+      //   "npm run lint",
+      //   "git add"
+      // ]
+
+
 /* scroll to init */
 
-var scroll = new SmoothScroll(".nav-link");
+var scroll = new SmoothScroll('.nav-link');
 
 /* modal window init */
 
@@ -9,118 +17,119 @@ var modal = new VanillaModal.default();
 /* get all links that has Modal and if they have Modal in href property and 
        add to them event listener that when clicks initialize tiny slider  */
 
-      var a = document.querySelectorAll("a");
+var a = document.querySelectorAll('a');
 
 for (var j = 0; j < a.length; j++) {
-  if (a[j].href.indexOf("Modal") !== -1) {
-    a[j].addEventListener("click", initTnsSlider);
+  if (a[j].href.indexOf('Modal') !== -1) {
+    a[j].addEventListener('click', initTnsSlider);
   }
 }
 /* init tns slider when click on link */
 
 function initTnsSlider() {
-  var index = this.href.indexOf("#");
+  var index = this.href.indexOf('#');
   var linkToModal = this.href.substr(index);
-  var queryContainer = linkToModal + " .carousel";
-  var carousel = document.querySelector(linkToModal + " .carousel");
-  tns({ container: carousel, nav: false, controlsText: ["", ""] });
+  var queryContainer = linkToModal + ' .carousel';
+  var carousel = document.querySelector(linkToModal + ' .carousel');
+  tns({ container: carousel, nav: false, controlsText: ['', ''] });
 }
 
 /* fade in #back-top */
 
-var toTopElement = document.getElementById("dynamic-to-top");
+var toTopElement = document.getElementById('dynamic-to-top');
 
-window.addEventListener("scroll", function() {
+window.addEventListener('scroll', function() {
   if (window.pageYOffset > 400) {
-    toTopElement.classList.remove("hide");
+    toTopElement.classList.remove('hide');
   } else {
-    toTopElement.classList.add("hide");
+    toTopElement.classList.add('hide');
   }
 });
 
 /* show hide menu on mobile */
 
-var btn = document.querySelector(".navbar-toggler");
+var btn = document.querySelector('.navbar-toggler');
 
 if (btn) {
-  menu = document.querySelector(".collapse");
-  btn.addEventListener("click", function() {
-    menu.classList.toggle("collapse");
+  menu = document.querySelector('.navbar-collapse');
+  btn.addEventListener('click', function() {
+    menu.classList.toggle('is-open');
+    this.classList.toggle('close');
   });
 }
 
 /* form validate */
 
 (function() {
-  var contactForm = document.getElementById("contact-form");
-  var nameField = document.getElementById("contact-name");
-  var emailField = document.getElementById("contact-email");
-  var textField = document.getElementById("contact-message");
-  var formBtn = document.getElementById("contact-send");
+  var contactForm = document.getElementById('contact-form');
+  var nameField = document.getElementById('contact-name');
+  var emailField = document.getElementById('contact-email');
+  var textField = document.getElementById('contact-message');
+  var formBtn = document.getElementById('contact-send');
 
   var validRegExp = {
     email: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    name: /^[а-яА-яіІЇїЄєґҐёЁA-Za-z]+$/
+    name: /^[а-яА-яіІЇїЄєґҐёЁA-Za-z]+$/,
   };
 
   var hasError = function(field) {
-    if (field.type === "button" || field.type === "submit") {
+    if (field.type === 'button' || field.type === 'submit') {
       return;
     }
 
     var emailTest = validRegExp.email.test(field.value);
     var nameTest = validRegExp.name.test(field.value);
 
-    if (field.value === "") {
-      return "Будь ласка заповніть поле";
+    if (field.value === '') {
+      return 'Будь ласка заповніть поле';
     }
-    if (field.name === "field-name" && !nameTest) {
+    if (field.name === 'field-name' && !nameTest) {
       return "Ви ввели неправильне ім'я";
     }
-    if (field.name === "field-email" && !emailTest) {
-      return "Ви ввели невірний email";
+    if (field.name === 'field-email' && !emailTest) {
+      return 'Ви ввели невірний email';
     }
   };
 
   var showError = function(field, error) {
-    field.classList.add("form-error");
+    field.classList.add('form-error');
     var id = field.id || field.name;
 
     if (!id) return;
-    var message = field.form.querySelector(".error-message#error-for-" + id);
+    var message = field.form.querySelector('.error-message#error-for-' + id);
 
     if (!message) {
-      message = document.createElement("div");
-      message.className = "error-message";
-      message.id = "error-for-" + id;
+      message = document.createElement('div');
+      message.className = 'error-message';
+      message.id = 'error-for-' + id;
       field.parentNode.insertBefore(message, field.nextSibling);
     }
 
-    field.setAttribute("aria-describedby", "error-for-" + id);
+    field.setAttribute('aria-describedby', 'error-for-' + id);
 
     message.innerHTML = error;
 
-    message.style.display = "block";
-    message.style.visibility = "visible";
+    message.style.display = 'block';
+    message.style.visibility = 'visible';
   };
 
   var removeError = function(field) {
-    field.classList.remove("form-error");
-    field.removeAttribute("aria-describedby");
+    field.classList.remove('form-error');
+    field.removeAttribute('aria-describedby');
 
     var id = field.id || field.name;
     if (!id) return;
 
-    var message = field.form.querySelector(".error-message#error-for-" + id);
+    var message = field.form.querySelector('.error-message#error-for-' + id);
     if (!message) return;
 
-    message.innerHTML = "";
-    message.style.display = "none";
-    message.style.visibility = "hidden";
+    message.innerHTML = '';
+    message.style.display = 'none';
+    message.style.visibility = 'hidden';
   };
 
   contactForm.addEventListener(
-    "blur",
+    'blur',
     function(event) {
       var error = hasError(event.target);
       if (error) {
@@ -129,10 +138,10 @@ if (btn) {
       }
       removeError(event.target);
     },
-    true
+    true,
   );
 
-  document.addEventListener("submit", function(event) {
+  document.addEventListener('submit', function(event) {
     var fields = event.target.elements;
     var fieldsLen = fields.length;
 
